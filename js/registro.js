d0b4ce791a200e122d1300f7b90615a3a5d8ac85
GerardGekyo <gerard.araq@gmail.com>
2025-07-17 18:41:47 -0500

    Pagina registro

---
+++ js/registro.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyASpKybXd9xllzP-nU1dp2kUGtxmIGOT7g",
+  authDomain: "eshop-ae14c.firebaseapp.com",
+  projectId: "eshop-ae14c",
+  storageBucket: "eshop-ae14c.firebasestorage.app",
+  messagingSenderId: "468037307240",
+  appId: "1:468037307240:web:310f28694fedeb3a48e4dd",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
